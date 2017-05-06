d5eb01dd62777075cebe383aaa5aee9c30244d8a
Nerugui <anton_mantini@hotmail.com>
2017-05-06 10:02:10 -0400

    Timer added
    Added a functional timer. The player has 45 seconds to decide what thet
    want to do. If they don't finish their turn in time, it will skip to the
    next player.

---
+++ TOJam2017/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text counterText;
+
+    public float seconds = 45f;
+
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+    }
+
+    void Update()
+    {
+        //seconds -= (int)Time.deltaTime;
+        seconds -= (Time.deltaTime);
+        counterText.text = seconds.ToString("00");
+    }
+
+    void ResetTime()
+    {
+        seconds = 45f;
+    }
+
+    void RanOutOfTime()
+    {
+        if (seconds == 0)
+        {
+            GameMaster.gameMode.AdvanceTurn();
+        }
+    }
+
+}
