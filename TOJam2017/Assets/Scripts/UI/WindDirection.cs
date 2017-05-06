ae73b7b2085d0d50488bbad17ec44f20fd48d142
Nerugui <anton_mantini@hotmail.com>
2017-05-06 13:32:20 -0400

    Wind direction and force text added and functional
    Base functionality of the wind and text are function on a UI basis.

---
+++ TOJam2017/Assets/Scripts/UI/WindDirection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindDirection : MonoBehaviour {
+
+    RectTransform rectTransform;
+    //randomized angle of the new wind
+    //*******************************NOTES******************************
+    // 0 = east, 90 = north, 180 = west, 270 = south, 360 = east
+    //******************************************************************
+    public int angle;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+        //For Testing purposes
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ResetWind();
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            RandomWind();
+        }
+    }
+
+    void RandomWind()
+    {
+        //randomizes the winds angle
+        angle = Random.Range(0, 360);
+        rectTransform.transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+
+    //Resets the direction back to a facing east. 
+    void ResetWind()
+    {
+        rectTransform.transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+
+}
