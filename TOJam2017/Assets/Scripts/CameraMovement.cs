5ea1216319c10bd8c0027285f51ea3cd956caaf0
Chenz13 <vincerauti@gmail.com>
2017-05-06 22:20:25 -0400

    Zoom Added
    Mouse Wheel zooming

--- TOJam2017/Assets/Scripts/CameraMovement.cs
+++ TOJam2017/Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,7 @@ public class CameraMovement : MonoBehaviour
 {
 
     //MemberVariables
+    public float zoomFactor, zoomSpeed, minZoom, maxZoom;
     private bool isFollowing;
     public bool notSkitty;
     public bool skitty1;
@@ -11,12 +12,14 @@ public class CameraMovement : MonoBehaviour
     float moveSpeed = 5f;
     Vector3 position; // center of screen
     Quaternion rotation;
-    
-                      // float FOV;
     Transform followTransform;
 
     private void Awake()
     {
+        minZoom = 2.5f;
+        maxZoom = 6.5f;
+        zoomFactor = 5;
+        zoomSpeed = 5;
         isFollowing = false;
         notSkitty = false;
     }
@@ -48,33 +51,40 @@ public class CameraMovement : MonoBehaviour
                 isFollowing = false;
             }
         }
+        HandleZoom();
         transform.position = position;
-       // transform.rotation = rotation;
+    }
+    void HandleZoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0.0f)
+        {
+            print(scroll);
+            zoomFactor -= scroll * zoomSpeed;
+            zoomFactor = Mathf.Clamp(zoomFactor, minZoom, maxZoom);
+        }
+
+       Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, zoomFactor, zoomSpeed * Time.deltaTime);
     }
     void HandleScrollMovement()
     {
         float mouseX = Input.mousePosition.x - (screenWidth / 2); //mouse position from the screenCenter as 0,0;
         float mouseY = Input.mousePosition.y - (screenHeight / 2); //..
-        //print(mouseX); 
-        //print(mouseY);
-        if (mouseX > (0.95 * screenWidth) / 2)
+
+        if (mouseX > (0.95 * screenWidth) / 2)                  
         {
-            print("MoveRight");
             position.x += 1.0f * moveSpeed * Time.deltaTime;
         }
         if (mouseX < -((0.95 * screenWidth) / 2))
         {
-            print("MoveLeft");
             position.x -= 1.0f * moveSpeed * Time.deltaTime;
         }
         if (mouseY > (0.95 * screenHeight) / 2)
         {
-            print("MoveUp");
             position.y += 1.0f * moveSpeed * Time.deltaTime;
         }
         if (mouseY < -((0.95 * screenHeight) / 2))
         {
-            print("MoveDown");
             position.y -= 1.0f * moveSpeed * Time.deltaTime;
         }
 
@@ -86,7 +96,6 @@ public class CameraMovement : MonoBehaviour
 
         if (transformToFollow != null)
         {
-       
             SnapToPosition(transformToFollow.position);
             rotation = transformToFollow.rotation;
             followTransform = transformToFollow;
@@ -94,13 +103,12 @@ public class CameraMovement : MonoBehaviour
         else
         {
             StopFollowing();
-            followTransform = null;
+            
         }
     }
     public void StopFollowing()
     {
         isFollowing = false;            // start updating camera edge movement.
-       // followTransform = null;
     }
 
     public void SnapToPosition(Vector3 targetPosition)
